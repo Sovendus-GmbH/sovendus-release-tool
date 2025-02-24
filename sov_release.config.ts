fc2609e1eb17d95fdf412547aaf80177cd020ded
Marcus Brandstaetter <marcus.brandstaetter@sovendus.com>
2025-02-24 20:13:41 +0100

    Release: 1.0.17

--- sov_release.config.ts
+++ sov_release.config.ts
@@ -4,7 +4,7 @@ const releaseConfig: ReleaseConfig = {
   packages: [
     {
       directory: "./",
-      version: "1.0.16",
+      version: "1.0.17",
       release: true,
       updateDeps: true,
       releaseOptions: {
