0d8583a328878cf98e7c3dd5fa18910aa8290a98
Marcus Brandstaetter <marcus.brandstaetter@sovendus.com>
2025-02-27 09:41:36 +0100

    Bump version to 1.0.26 in package.json and sov_release.config.ts

--- sov_release.config.ts
+++ sov_release.config.ts
@@ -4,7 +4,7 @@ const releaseConfig: ReleaseConfig = {
   packages: [
     {
       directory: "./",
-      version: "1.0.25",
+      version: "1.0.26",
       release: true,
       updateDeps: true,
       releaseOptions: {
