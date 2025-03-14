087000f5c19c11608366e9a7e127187be9013394
Marcus Brandstaetter <marcus.brandstaetter@sovendus.com>
2025-03-14 11:28:15 +0100

    Release: 1.4.0

--- sov_release.config.ts
+++ sov_release.config.ts
@@ -9,7 +9,7 @@ const releaseConfig: ReleaseConfig = {
       build: true,
       test: true,
       release: {
-        version: "1.3.0",
+        version: "1.4.0",
         foldersToScanAndBumpThisPackage: [
           // scan the whole dev env folder
           { folder: "../../" },
